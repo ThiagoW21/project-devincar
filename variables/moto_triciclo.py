d67ac063e191f668625ece3a96343f3d6c10f873
Thiago William <thiagokepler1@gmail.com>
2022-07-07 19:16:04 -0300

    feat/Das variáveis com de moto/triciclo

---
+++ variables/moto_triciclo.py
@@ -0,0 +1,9 @@
+from classes.moto_triciclo import MotoTriciclo
+
+
+moto_triciclo = {
+    'CG_160_start': MotoTriciclo('13/02/2020', 'CG 160 Start', 'NEV-7450', 12000, 'Preto', 100, 2, 'gasolina'),
+    'CG_160_fan': MotoTriciclo('22/06/2021', 'CG 160 Fan', 'HTG-3337', 13480, 'Azul', 120, 2, 'gasolina'),
+    'POP100_i': MotoTriciclo('11/04/2019', 'POP 100 i', 'HWO-1382', 8330, 'Branc0', 100, 2, 'gasolina'),
+    'PCX': MotoTriciclo('04/12/2022', 'PCX', 'JSW-9323', 14690, 'Azul', 100, 2, 'gasolina')
+}
